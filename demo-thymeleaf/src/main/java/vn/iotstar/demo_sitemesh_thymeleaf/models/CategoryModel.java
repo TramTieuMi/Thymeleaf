564db3c731e69a99e1dc660a257742d44f11153a
TramTieuMi <157827317+TramTieuMi@users.noreply.github.com>
2024-10-30 10:09:26 +0700

    ad
    a

---
+++ demo-thymeleaf/src/main/java/vn/iotstar/demo_sitemesh_thymeleaf/models/CategoryModel.java
@@ -0,0 +1,35 @@
+package vn.iotstar.demo_sitemesh_thymeleaf.models;
+
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CategoryModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    private Long id;
+
+    @NotEmpty(message = "Không được bỏ trống")
+    private String name;
+
+    private String images;
+
+    private int status;
+
+    private Boolean setIsEdit=false;
+    private boolean edit;
+    public boolean setIsEdit() {
+        return edit;
+    }
+
+    public void setIsEdit(boolean edit) {
+        this.edit = edit;
+    }
+}
